0af3c7357d2d24109a1dafd917c9427a4e4513a0
Giorgi Isaiashvili <giorgi.isaiashvili@gmail.com>
2020-04-20 19:30:41 +0400

    added ProductItem with styles

--- screens/shop/ProductsOverviewScreen.tsx
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -3,6 +3,8 @@ import { View, StyleSheet, FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/root';
 import Product from '../../models/product';
+import ProductItem from '../../components/shop/ProductItem';
+import { State } from 'react-native-gesture-handler';
 
 export interface Props {
 
@@ -13,11 +15,17 @@ export const ProductsOverviewScreen: React.FC<Props> = () => {
     const products = useSelector<RootState, Product[]>(state => state.products.availableProducts);
 
     return (
-        <View style={styles.container}>
-            <FlatList data={products}
-                renderItem={itemData => <Text>{itemData.item.title}</Text>}
-            />
-        </View>
+        <FlatList
+            data={products}
+            renderItem={itemData =>
+                <ProductItem
+                    title={itemData.item.title}
+                    imageUri={itemData.item.imageUrl}
+                    price={itemData.item.price}
+                    onToCartClick={() => { }}
+                    onViewDetailsClick={() => { }}
+                />}
+        />
     )
 };
 
